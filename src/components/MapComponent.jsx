90ed888906e75fad744c2b5489782c9494245d10
Camila Sosa <camila.sosa@neoris.com>
2020-05-09 14:07:28 -0300

    Pantalla prototipo mapa terminada

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,14 +1,45 @@
+/* global google */
 import React from 'react'
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, DirectionsRenderer } from 'react-google-maps'
+import '../styles/MapComponent.css'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, DirectionsRenderer} from 'react-google-maps'
+import { Button } from '@blueprintjs/core'
 
 class MapComponent extends React.Component {
 
   constructor(props){
     super(props)
     this.state = {
-      home: {lat: -34.705081, lng: -58.279033},
-      shop: {lat: -34.712337, lng: -58.283670}
+      home: {lat: -34.7058979, lng: -58.2775644},
+      shop: {lat: -34.7116032, lng: -58.2701079},
+      directions:  {},
+      directionsInfo: {},
+      showDestinations: true
     }
+    this.showDirections = this.showDirections.bind(this);
+  }
+
+  showDirections(){
+    const directionsService = new google.maps.DirectionsService();
+
+    directionsService.route(
+      {
+        origin: this.state.home,
+        destination: this.state.shop,
+        travelMode: google.maps.TravelMode.WALKING
+      },
+      (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          this.setState({
+            directions: result,
+            showDestinations: false,
+            directionsInfo: result.routes[0].legs[0]
+          });
+          console.log(this.state.directionsInfo)
+        } else {
+          console.error(`error fetching directions ${result}`);
+        }
+      }
+    );
   }
 
   render(){
@@ -18,23 +49,28 @@ class MapComponent extends React.Component {
         <GoogleMap defaultZoom={15} defaultCenter={this.state.home}>
           {props.isMarkerShown && 
           <div>
-            <Marker position={this.state.home} />
-            <Marker position={this.state.shop} />
-            <DirectionsRenderer directions={this.state.directions}/>
+            {this.state.showDestinations && <Marker position={this.state.home} />}
+            {this.state.showDestinations && <Marker position={this.state.shop} />}
+            {<DirectionsRenderer directions={this.state.directions}/>}
         </div>}
         </GoogleMap>
     ))
 
-    // keyString: key=AIzaSyBIdMK9gkh8Na2UxQ_xeOi4fx_PNz9Fd5g&
-
     return (
       <div>
         <div>
+        <Button onClick={this.showDirections}>Mostrar Ruta</Button>
+        {!this.state.showDestinations && 
+          <div>
+            <p>Información:</p>
+            <p>Distancia: {this.state.directionsInfo.distance.text}</p>
+            <p>Tiempo (a pie): {this.state.directionsInfo.duration.text}</p>
+          </div>}
         <MyMapComponent isMarkerShown
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-          loadingElement={<div style={{ height: '500px', width: '800px' }} />}
-          containerElement={<div style={{ height: '500px', width: '800px' }} />}
-          mapElement={<div style={{ height: '500px', width: '800px' }} />}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAnJpYCinVV03Jop32Qzwp759fOVBXiSc8&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div className="map-size" />}
+          containerElement={<div className="map-size" />}
+          mapElement={<div className="map-size" />}
         />
         </div>
       </div>
