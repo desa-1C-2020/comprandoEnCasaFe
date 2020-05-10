dc5e01feed9f855af2c41ed461c3b59104b3743d
Camila Sosa <camila.sosa@neoris.com>
2020-05-10 17:15:49 -0300

    Estilos para el buscador de comercios

--- src/components/ShopSearch.jsx
+++ src/components/ShopSearch.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import '../styles/HomeNavBar.css'
+import '../styles/ShopSearch.css'
 import { InputGroup, Button } from '@blueprintjs/core'
 import SearchResult from './SearchResult';
-
 
 export class ShopSearch extends React.Component {
 
@@ -10,12 +9,17 @@ export class ShopSearch extends React.Component {
     super();
     this.state = {
       showResults: false,
-      lat: '-34.7058979',
-      lng: '-58.2775644',
+      lat: '-34.7040003',
+      lng: '-58.2754042',
       max: '2000'
     }
     this.openResultsModal = this.openResultsModal.bind(this)
     this.closeResultsModal = this.closeResultsModal.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event){
+    this.setState({[event.target.name]: event.target.value})
   }
 
   openResultsModal(){
@@ -28,17 +32,37 @@ export class ShopSearch extends React.Component {
 
 	render() {
     return (
-      <div style={{ maxWidth: '500px', borderRadius: '20px'}}>
-        <h1 style={{color: 'white'}}>Buscar comercios</h1>
-        <h2 style={{color: 'white'}}>Mi domicilio</h2>
-        <div>
-          <InputGroup type="text" placeholder="Longitud"></InputGroup>
-          <InputGroup type="text" placeholder="Latitud"></InputGroup>
+      <div style={{paddingLeft: '20px', paddingRight: '20px'}}>
+        <div className="background">
+          <h1 className="title">Buscar comercios</h1>
+          <h2 className="title">Mi domicilio</h2>
+          <div className="input-container">
+            <InputGroup className="input" 
+                        type="text" 
+                        name="lgn"
+                        value={this.state.lng}
+                        onChange={this.handleChange} 
+                        placeholder="Longitud"
+            />
+            <InputGroup className="input" 
+                        type="text" 
+                        name="lat"
+                        value={this.state.lat} 
+                        onChange={this.handleChange}
+                        placeholder="Latitud"
+            />
+          </div>
+          <h2 className="title">Distancia máxima (en metros)</h2>
+          <InputGroup className="input" 
+                      type="number" 
+                      name="max"
+                      value={this.state.max} 
+                      onChange={this.handleChange}
+                      placeholder="Metros de distancia"
+          />
+          <Button className="search-button" onClick={this.openResultsModal} icon='search'>Buscar</Button>
+          {this.state.showResults && <SearchResult isOpen={this.state.showResults} closeModal={this.closeResultsModal} {...this.state}/>}
         </div>
-        <h2 style={{color: 'white'}}>Distancia máxima (en metros)</h2>
-        <InputGroup type="number" placeholder="Max metros de distancia"></InputGroup>
-        <Button onClick={this.openResultsModal}>Buscar</Button>
-        {this.state.showResults && <SearchResult isOpen={this.state.showResults} closeModal={this.closeResultsModal} {...this.state}/>}
       </div>
 		);
 	}
