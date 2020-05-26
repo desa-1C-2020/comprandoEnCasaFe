477f389b6ba74fffaff4cb68cf7273b6e2e03f67
Camila Sosa <camila.sosa@neoris.com>
2020-05-26 17:19:35 -0300

    Estilos registro terminados

--- src/components/SellerForm.jsx
+++ src/components/SellerForm.jsx
@@ -27,75 +27,78 @@ export class SellerForm extends React.Component {
 	render() {
     return (
       <div>
-        <p>Datos del comercio</p>
+        <p className="ddc">Datos del comercio</p>
         <span className="shop">
-        <InputGroup className="input" 
-                        type="text" 
-                        name="name"
-                        value={this.state.name}
-                        onChange={this.handleChange} 
-                        placeholder="Nombre"
-            />
-        <InputGroup className="input" 
-                        type="text" 
-                        name="sector"
-                        value={this.state.sector}
-                        onChange={this.handleChange} 
-                        placeholder="Rubro"
-            />
+          <InputGroup className="field" 
+                      type="text" 
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.handleChange} 
+                      placeholder="Nombre"
+          />
+          <InputGroup className="field" 
+                      type="text" 
+                      name="sector"
+                      value={this.state.sector}
+                      onChange={this.handleChange} 
+                      placeholder="Rubro"
+          />
         </span>
-          <p>Dirección del comercio</p>
+        <p>Dirección del comercio</p>
         <span className="address">
-        <InputGroup className="input" 
-                        type="text" 
-                        name="address"
-                        value={this.state.address}
-                        onChange={this.handleChange} 
-                        placeholder="Dirección"
-            />
-        <InputGroup className="input" 
-                        type="text" 
-                        name="lat"
-                        value={this.state.lat}
-                        onChange={this.handleChange} 
-                        placeholder="Latitud"
-            />
-      <InputGroup className="input" 
-                        type="text" 
-                        name="lng"
-                        value={this.state.lng}
-                        onChange={this.handleChange} 
-                        placeholder="Longitud"
-            />
-            </span>
-      <p>Formas de pago</p>
-      <span className="payments">
-      <Checkbox checked={this.state.money}
-                 onChange={() => this.setState({money: !this.state.money})}>
-                   Efectivo
-      </Checkbox>
-      <Checkbox  checked={this.state.debit}
-                 onChange={() => this.setState({debit: !this.state.debit})}>
-                   Tarjetas de débito
-      </Checkbox>
-      <Checkbox  checked={this.state.credit}
-                 onChange={() => this.setState({credit: !this.state.credit})}>
-                  Tarjetas de crédito
-      </Checkbox>
-      </span>
-      <div>
-        <Button>Personalizar horarios</Button>
-      </div>
-      <p>Distancia máxima de entregas (en metros)</p>
-      <span className="range">
-      <InputGroup className="input" 
-                        type="number" 
-                        name="radio"
-                        value={this.state.radio}
-                        onChange={this.handleChange} 
-                        placeholder="Ej: 2000"
-            />
-      </span>
+          <InputGroup className="field" 
+                      type="text" 
+                      name="address"
+                      value={this.state.address}
+                      onChange={this.handleChange} 
+                      placeholder="Dirección"
+          />
+          <InputGroup className="field" 
+                      type="text" 
+                      name="lat"
+                      value={this.state.lat}
+                      onChange={this.handleChange} 
+                      placeholder="Latitud"
+          />
+          <InputGroup className="field" 
+                      type="text" 
+                      name="lng"
+                      value={this.state.lng}
+                      onChange={this.handleChange} 
+                      placeholder="Longitud"
+          />
+        </span>
+        <p>Formas de pago</p>
+        <span className="payments">
+          <Checkbox className="check"
+                    checked={this.state.money}
+                    onChange={() => this.setState({money: !this.state.money})}>
+                    Efectivo
+          </Checkbox>
+          <Checkbox className="check"
+                    checked={this.state.debit}
+                    onChange={() => this.setState({debit: !this.state.debit})}>
+                    Tarjetas de débito
+          </Checkbox>
+          <Checkbox className="check"
+                    checked={this.state.credit}
+                    onChange={() => this.setState({credit: !this.state.credit})}>
+                    Tarjetas de crédito
+          </Checkbox>
+        </span>
+        <div className="seller-btn">
+          <Button>Personalizar horarios</Button>
+        </div>
+        <p className="d-max">Distancia máxima de entregas (en metros)</p>
+        <span className="range">
+          <InputGroup className="field" 
+                      type="number" 
+                      name="radio"
+                      value={this.state.radio}
+                      onChange={this.handleChange} 
+                      placeholder="Ej: 2000"
+          />
+        </span>
       </div>
 		);
 	}
