f7c537c31c00e991127ddba9cbd54cd6c3889850
Camila Sosa <camila.sosa@neoris.com>
2020-06-15 20:23:07 -0300

    Endpoint obtener productos

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -12,47 +12,47 @@ export function searchForShops(lat, lng, max, callback) {
 
 export function searchProduct(text, callback){
   //TODO - path para obtener productos???
-/*  axios.get(`${host}/?????`)
+  axios.get(`${host}/?????`)
   .then((response) => {
-    callback(null, response)   
+    callback(null, response.data)   
   })
-  .catch((error) => callback(error, null)) */
-  //TODO - eliminar estos mocks
-  let res = {
-  products: [
-    {
-      price: '70.00',
-      product: {
-        name: 'Aceite 2L',
-        brand: 'Marolio',
-        image: 'https://www.supermercadoacuario.com.ar/app/files/company_35/products/66557_7797470005514.jpg'
-      }
-    },
-    {
-      price: '32.00',
-      product: {
-        name: 'Puré de tomate',
-        brand: 'De la huerta',
-        image: 'https://mercanet.com.ar/server/Portal_0019782/img/products/pure-de-tomate-de-la-huerta-530-grs_9308754.jpg'
-      }
-    },
-    {
-      price: '10.99',
-      product: {
-        name: 'Harina 0000',
-        brand: 'Pureza',
-        image: 'https://mayoristaencountry.com/25204-large_default/harina-pureza-4-0-x-1-kg-1-kg.jpg'
-      }
-    },
-    {
-      price: '250.00',
-      product: {
-        name: 'Yerba 1K',
-        brand: 'Playadito',
-        image: 'https://lapulperia.com.ar/presta17/366-home_default/yerba-playadito-x-500-gr.jpg'
-      }
-    }
-  ]
-  }
-  callback(null, res);
+  .catch((error) => callback(error, null)) 
+  //TODO - eliminar estos mocks cuando sea necesario
+  // let res = {
+  // products: [
+  //   {
+  //     price: '70.00',
+  //     product: {
+  //       name: 'Aceite 2L',
+  //       brand: 'Marolio',
+  //       image: 'https://www.supermercadoacuario.com.ar/app/files/company_35/products/66557_7797470005514.jpg'
+  //     }
+  //   },
+  //   {
+  //     price: '32.00',
+  //     product: {
+  //       name: 'Puré de tomate',
+  //       brand: 'De la huerta',
+  //       image: 'https://mercanet.com.ar/server/Portal_0019782/img/products/pure-de-tomate-de-la-huerta-530-grs_9308754.jpg'
+  //     }
+  //   },
+  //   {
+  //     price: '10.99',
+  //     product: {
+  //       name: 'Harina 0000',
+  //       brand: 'Pureza',
+  //       image: 'https://mayoristaencountry.com/25204-large_default/harina-pureza-4-0-x-1-kg-1-kg.jpg'
+  //     }
+  //   },
+  //   {
+  //     price: '250.00',
+  //     product: {
+  //       name: 'Yerba 1K',
+  //       brand: 'Playadito',
+  //       image: 'https://lapulperia.com.ar/presta17/366-home_default/yerba-playadito-x-500-gr.jpg'
+  //     }
+  //   }
+  // ]
+  // }
+  // callback(null, res);
 }
