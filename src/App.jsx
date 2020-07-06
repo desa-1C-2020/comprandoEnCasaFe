bd4cea1c42b1d3a8829b2af7273d5cb581f9e88e
Marcos <marcos.alvarenga@10pines.com>
2020-07-06 00:47:41 -0300

    Add app e index pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,90 @@
-import React from 'react'
-import HomeScreen from './screens/HomeScreen'
-import DeliveryScreen from './screens/DeliveryScreen'
-import LogInScreen from './screens/LogInScreen'
-import RegisterScreen from './screens/RegisterScreen'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React, { Component } from 'react';
+import HomeScreen from './screens/HomeScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
+import Login from './screens/user/login/Login';
+import RegisterScreen from './screens/RegisterScreen';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { getCurrentUser } from './util/APIUtils';
+import { ACCESS_TOKEN } from './constants/index';
+import LoadingIndicator from './common/LoadingIndicator';
+import AppHeader from './common/AppHeader';
+import PrivateRoute from './common/PrivateRoute';
+import NotFound from './common/NotFound';
+import Alert from 'react-s-alert';
+import './App.css';
+import OAuth2RedirectHandler from '../src/screens/user/oauth2/OAuth2RedirectHandler';
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            authenticated: false,
+            currentUser: null,
+            loading: false
+        };
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={LogInScreen} />
-        <Route exact path='/register' component={RegisterScreen} />
-        <Route exact path='/home' component={HomeScreen} />
-        <Route exact path='/delivery' component={DeliveryScreen} />
-      </Switch>
-    </Router>
-  )
+        this.loadCurrentlyLoggedInUser = this.loadCurrentlyLoggedInUser.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    loadCurrentlyLoggedInUser() {
+        this.setState({
+            loading: true
+        });
+
+        getCurrentUser()
+            .then(response => {
+                this.setState({
+                    currentUser: response,
+                    authenticated: true,
+                    loading: false
+                });
+            }).catch(error => {
+            this.setState({
+                loading: false
+            });
+        });
+    }
+
+    handleLogout() {
+        localStorage.removeItem(ACCESS_TOKEN);
+        this.setState({
+            authenticated: false,
+            currentUser: null
+        });
+        Alert.success('You\'re safely logged out!');
+    }
+
+    componentDidMount() {
+        this.loadCurrentlyLoggedInUser();
+    }
+
+    render() {
+        if (this.state.loading) {
+            return <LoadingIndicator/>;
+        }
+
+        return (
+            <div className="app">
+                <div className="app-top-box">
+                    <AppHeader authenticated={this.state.authenticated} onLogout={this.handleLogout}/>
+                </div>
+                <div className="app-body">
+                    <Switch>
+                        <Route path="/"
+                               render={(props) => <Login
+                                   authenticated={this.state.authenticated} {...props} />}></Route>
+                        <Route exact path='/home' component={HomeScreen}/>
+                        <Route path="/oauth2/redirect" component={OAuth2RedirectHandler}></Route>
+                        <Route component={NotFound}></Route>
+                    </Switch>
+                </div>
+                <Alert stack={{ limit: 3 }}
+                       timeout={3000}
+                       position='top-right' effect='slide' offset={65}/>
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Container from './Container'
-import '@blueprintjs/core/lib/css/blueprint.css'
-import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from './Container';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Container/>, document.getElementById('root'))
+ReactDOM.render(
+    <Router>
+        <Container/>
+    </Router>,
+    document.getElementById('root')
+);
