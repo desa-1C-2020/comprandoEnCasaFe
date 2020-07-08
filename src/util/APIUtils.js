0ca06fd0ec7dab9801670fe783c42c7c6c40cb52
Marcos <marcos.alvarenga@10pines.com>
2020-07-08 00:57:27 -0300

    Obtiene el usuario logueado.

--- src/util/APIUtils.js
+++ src/util/APIUtils.js
@@ -1,42 +1,65 @@
-import { API_BASE_URL, ACCESS_TOKEN } from '../constants';
+import { ACCESS_TOKEN, API_BASE_URL, BUYER, SELLER } from '../constants';
 
 const request = (options) => {
     const headers = new Headers({
-        'Content-Type': 'application/json',
-    })
-    
-    if(localStorage.getItem(ACCESS_TOKEN)) {
-        headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
+        'Content-Type': 'application/json'
+    });
+
+    if (localStorage.getItem(ACCESS_TOKEN)) {
+        headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN));
     }
 
-    const defaults = {headers: headers};
+    const defaults = { headers: headers };
     options = Object.assign({}, defaults, options);
 
     return fetch(options.url, options)
-    .then(response => 
-        response.json().then(json => {
-            if(!response.ok) {
-                return Promise.reject(json);
-            }
-            return json;
-        })
-    );
+        .then(response =>
+            response.json().then(json => {
+                if (!response.ok) {
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        );
 };
 
 export function getCurrentUser() {
-    if(!localStorage.getItem(ACCESS_TOKEN)) {
-        return Promise.reject("No access token set.");
+    if (!localStorage.getItem(ACCESS_TOKEN)) {
+        return Promise.reject('No access token set.');
     }
 
     return request({
-        url: API_BASE_URL + "/user/me",
+        url: API_BASE_URL + '/user/me',
         method: 'GET'
+    }).then((user) => {
+        const accountType = user.commerce === undefined ? BUYER : SELLER;
+        return {
+            accountType,
+            user
+        };
+    });
+}
+
+export function getAllProducts() {
+    if (!localStorage.getItem(ACCESS_TOKEN)) {
+        return Promise.reject('No access token set.');
+    }
+
+    return request({
+        url: API_BASE_URL + '/user/me',
+        method: 'GET'
+    }).then((user) => {
+        const accountType = user.commerce === undefined ? BUYER : SELLER;
+        return {
+            accountType,
+            user
+        };
     });
 }
 
 export function login(loginRequest) {
     return request({
-        url: API_BASE_URL + "/auth/login",
+        url: API_BASE_URL + '/auth/login',
         method: 'POST',
         body: JSON.stringify(loginRequest)
     });
@@ -44,7 +67,7 @@ export function login(loginRequest) {
 
 export function signup(signupRequest) {
     return request({
-        url: API_BASE_URL + "/auth/signup",
+        url: API_BASE_URL + '/auth/signup',
         method: 'POST',
         body: JSON.stringify(signupRequest)
     });
