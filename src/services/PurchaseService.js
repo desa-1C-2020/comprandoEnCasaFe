712e56c38c0b62a9310ff04fc8c78645f7d57c66
Marcos <marcos.alvarenga@10pines.com>
2020-07-14 01:56:29 -0300

    Fix month error

--- src/services/PurchaseService.js
+++ src/services/PurchaseService.js
@@ -3,25 +3,25 @@ import { secureRequest } from '../util/APIUtils';
 
 // TODO - en el back hay que cambiarlo a POST!
 export function takeawayOptions(takeAwayTo) {
-  return secureRequest({
-    url: API_BASE_URL + '/purchase/takeAway',
-    method: 'POST',
-    body: JSON.stringify(takeAwayTo)
-  });
+    return secureRequest({
+        url: API_BASE_URL + '/purchase/takeAway',
+        method: 'POST',
+        body: JSON.stringify(takeAwayTo)
+    });
 }
 
 export function deliveryOptions() {
-  return secureRequest({
-    url: API_BASE_URL + '/purchase/delivery',
-    method: 'GET'
-  });
+    return secureRequest({
+        url: API_BASE_URL + '/purchase/delivery',
+        method: 'GET'
+    });
 }
 
 // TODO - Hay que probarlo!
 export function sendPurchase(purchase) {
-  return secureRequest({
-    url: API_BASE_URL + '/purchase',
-    method: 'POST',
-    body: JSON.stringify(purchase)
-  });
+    return secureRequest({
+        url: API_BASE_URL + '/purchase',
+        method: 'POST',
+        body: JSON.stringify(purchase)
+    });
 }
